17de8fa1be48d2b7d0c81b40f0931904893e121d
snghojeong <snghojeong@gmail.com>
2018-08-07 09:35:38 +0900

    Implement observable::emit() method

--- observable.hpp
+++ observable.hpp
@@ -9,7 +9,11 @@ public:
   explicit observable() {}
   virtual ~observable() {}
 
-  virtual void emit() = 0;
+  virtual void emit() {
+    std::transform(_dat_list.begin(), _dat_list.end(), nullptr, [] (data& dat) {
+        std::transform(_obs_list.begin(), _obs_list.end(), nullptr, [] (observer& obs) { obs.notify(dat); });
+        });
+  }
   void add(observer& obs) { _obs_list.push_back(obs); }
   void add(data& dat) { _dat_list.push_back(dat); }
 
