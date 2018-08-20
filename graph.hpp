ca1ea450045e56845a235228bd53e6925446f3e7
snghojeong <snghojeong@gmail.com>
2018-08-20 11:30:54 +0900

    Add graph class

---
+++ graph.hpp
@@ -0,0 +1,18 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+namespace react_cpp {
+
+class graph {
+public:
+template <typename T>
+  T get() {
+    T isnt;
+    return inst;
+  }
+
+};
+
+};
+
+#endif
