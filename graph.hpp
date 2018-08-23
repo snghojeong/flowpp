cfa4a1c72f34db40f8abffd4e31e0791077f421a
snghojeong <snghojeong@gmail.com>
2018-08-23 16:59:28 +0900

    Implement constructor / destructor on graph class

--- graph.hpp
+++ graph.hpp
@@ -5,6 +5,9 @@ namespace react_cpp {
 
 class graph {
 public:
+  explicit graph() {}
+  virtual ~graph() { std::for_each(_obsbl_list.begin(), _obsbl_list.end(), [] (observable* obsbl) { delete obsbl; }); }
+
   template <typename T>
   T get() {
     T *inst = new T();
