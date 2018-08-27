75978856af8da388813643245adab0ee4c57cbfe
snghojeong <snghojeong@gmail.com>
2018-08-27 20:17:40 +0900

    Add counter example

---
+++ examples/counter.cpp
@@ -0,0 +1,29 @@
+class counter : public source {
+public:
+  explicit counter() { _cnt = 0; }
+  virtual ~counter() {}
+
+  data<unsigned int> generate() {
+    return data(_cnt++);
+  }
+
+  unsigned int get() {
+    return _cnt;
+  }
+
+private:
+  unsigned int _cnt;
+};
+
+int main()
+{
+  react_cpp::graph cnt_graph();
+
+  auto cnter = cnt_graph();
+
+  cnt_graph.wait(0 /* timeout */, 10 /* numnber of loop */);
+
+  std::cout << cnter.get(); << std::endl; // print 10
+
+  return 0;
+}
