f51611414abf077db867c35ac8bee4b2c5daed32
snghojeong <snghojeong@gmail.com>
2018-08-29 10:48:37 +0900

    Make observer friend of observable to get member

--- observer.hpp
+++ observer.hpp
@@ -1,15 +1,17 @@
 #ifndef _OBSERVER_HPP_
 #define _OBSERVER_HPP_
 
+#ifndef _OBSERVERABLE_HPP_
+#include <observable.hpp>
+#endif
+
 namespace react_cpp {
 
 class data;
-class observable;
-
 class observer {
 public:
   virtual void notify(data& dat) = 0;
-  void subscribe(observable& obsbl) { obsbl.add(this); }
+  void subscribe(observable& obsbl) { obsbl._obs_list.push_back(this); }
 };
 
 };
