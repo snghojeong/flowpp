777fa1cc64832c0cd5a6725de1f3da183df0e025
snghojeong <snghojeong@gmail.com>
2018-08-29 11:04:22 +0900

    Make data as a abstract parent class

--- data.hpp
+++ data.hpp
@@ -3,26 +3,10 @@
 
 namespace react_cpp {
 
-template <typename T>
-class data<T> {
+class data {
 public:
-  explicit data(T val) { _val = val; }
-  virtual ~data() {}
-
-  T value() { return _val; }
-
-  data& operator = (const T& operand) {
-    _var = operand;
-    return *this;
-  }
-
-  data& operator = (const data<T, F>& operand) {
-    _var = operand._var;
-    return *this;
-  }
-
-private:
-  T _val;
+  explicit data() { }
+  virtual ~data() { }
 };
 
 };
