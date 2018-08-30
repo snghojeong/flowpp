09eb7e3d484e23afbfaa74dbd0e35b589b4f4a8a
snghojeong <snghojeong@gmail.com>
2018-08-30 16:41:07 +0900

    Remove subscribe cuz of bi-directional refernce

--- observer.hpp
+++ observer.hpp
@@ -1,17 +1,13 @@
 #ifndef _OBSERVER_HPP_
 #define _OBSERVER_HPP_
 
-#ifndef _OBSERVERABLE_HPP_
-#include <observable.hpp>
-#endif
-
 namespace react_cpp {
 
 class data;
 class observer {
 public:
   virtual void notify(data& dat) = 0;
-  void subscribe(observable& obsbl) { obsbl._obs_list.push_back(this); }
+  //void subscribe(observable& obsbl) { obsbl._obs_list.push_back(this); }
 };
 
 };
