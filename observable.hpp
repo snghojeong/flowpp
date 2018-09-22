e13e6612d27bc8812572d1889b57d391a71ec074
snghojeong <snghojeong@gmail.com>
2018-09-22 22:54:47 +0900

    Use using for type aliasing on observable

--- observable.hpp
+++ observable.hpp
@@ -14,6 +14,9 @@
 namespace react_cpp {
 
 class observable {
+  using string = std::string;
+  using data_sptr = std::shared_ptr<data>;
+  using observer_sptr = std::shared_ptr<observer>;
   friend class observer;
 
 public:
@@ -21,21 +24,21 @@ public:
   virtual ~observable() {}
 
   virtual void emit() {
-    std::for_each(_dat_list.begin(), _dat_list.end(), [this] (std::shared_ptr<data> dat) {
-      std::for_each(_obs_map.begin(), _obs_map.end(), [=] (std::pair<std::string, std::shared_ptr<observer>> obs) { 
+    std::for_each(_dat_list.begin(), _dat_list.end(), [this] (data_sptr dat) {
+      std::for_each(_obs_map.begin(), _obs_map.end(), [=] (std::pair<string, observer_sptr> obs) { 
         if (this->compare(pair.first, dat->type()) pair.second->notify(*dat); 
       });
     });
   }
 
-  void add(std::shared_ptr<data> dat) { _dat_list.push_back(dat); }
-  void add(std::shared_ptr<observer> obs, std::string key = "*") { _obs_map.insert( { key, obs } ); }
+  void add(data_sptr dat) { _dat_list.push_back(dat); }
+  void add(observer_sptr obs, string key = "*") { _obs_map.insert( { key, obs } ); }
 
-  virtual bool compare(const std::string& key, const std::string& dat) = 0;
+  virtual bool compare(const string& key, const string& dat) = 0;
 
 private:
-  std::map<std::string, std::shared_ptr<observer>> _obs_map;
-  std::list<std::shared_ptr<data>> _dat_list;
+  std::map<string, observer_sptr> _obs_map;
+  std::list<data_sptr> _dat_list;
 };
 
 };
