14fd51c1da3fed2d528cfd9a4830bb012107bdea
Seungho Jeong <snghojeong@gmail.com>
2021-07-11 22:48:39 +0900

    Update observable.hpp

--- observable.hpp
+++ observable.hpp
@@ -15,8 +15,8 @@ namespace flowpp {
 
 class observable {
   using string = std::string;
-  using data_sptr = std::unique_ptr<data>;
-  using observer_sptr = std::unique_ptr<observer>;
+  using data_ptr = std::unique_ptr<data>;
+  using observer_ptr = std::unique_ptr<observer>;
   friend class observer;
 
 public:
@@ -24,19 +24,19 @@ public:
   virtual ~observable() {}
 
   virtual void process() {
-    std::for_each(_dat_list.begin(), _dat_list.end(), [this] (data_sptr dat) {
-      std::for_each(_obs_map.begin(), _obs_map.end(), [=] (std::pair<filter, observer_sptr> obs) { 
+    std::for_each(_dat_list.begin(), _dat_list.end(), [this] (data_ptr dat) {
+      std::for_each(_obs_map.begin(), _obs_map.end(), [=] (std::pair<filter, observer_ptr> obs) { 
         pair.second->notify(dat); 
       });
     });
   }
 
-  void add(data_sptr dat) { _dat_list.push_back(dat); }
-  void add(observer_sptr obs, filter fltr = filter()) { _obs_map.insert( { fltr, obs } ); }
+  void add(data_ptr dat) { _dat_list.push_back(dat); }
+  void add(observer_ptr obs, filter fltr = filter()) { _obs_map.insert( { fltr, obs } ); }
 
 private:
-  std::map<string, observer_sptr> _obs_map;
-  std::list<data_sptr> _dat_list;
+  std::map<string, observer_ptr> _obs_map;
+  std::list<data_ptr> _dat_list;
 };
 
 };
