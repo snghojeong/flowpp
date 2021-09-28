8d61a72fbbbf28eb146a48265d97381490d872d7
Seungho Jeong <snghojeong@gmail.com>
2021-09-28 22:41:51 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -1,16 +1,12 @@
 #ifndef _DATA_H_
 #define _DATA_H_
 
-namespace flowpp {
-
 class data {
 public:
   explicit data() { }
   virtual ~data() { }
 
   virtual const format& format() const = 0;
-};
-
 };
 
 #endif
