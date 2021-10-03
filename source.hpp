9f119e191b10f6cf8111d3670bd53e9b3b9b13a5
Seungho Jeong <snghojeong@gmail.com>
2021-10-03 23:11:18 +0900

    Update source.hpp

--- source.hpp
+++ source.hpp
@@ -1,8 +1,6 @@
 #ifndef _SOURCE_HPP_
 #define _SOURCE_HPP_
 
-namespace flowpp {
-
 class source : public observable {
   using data_uptr = std::unique_ptr<data>;
 
@@ -16,8 +14,6 @@ public:
   }
 
   virtual data_uptr generate() = 0; 
-};
-
 };
 
 #endif
