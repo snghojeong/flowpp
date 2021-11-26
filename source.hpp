923768020aaac1fcfab7bf2d23364ddac9a7c8d2
Seungho Jeong <snghojeong@gmail.com>
2021-11-26 23:27:29 +0900

    Update source.hpp

--- source.hpp
+++ source.hpp
@@ -4,6 +4,7 @@
 class source : public observable {
   using data_uptr = std::unique_ptr<data>;
   using data_sptr = std::shared_ptr<data>;
+  using format_uptr = std::unique_ptr<format>;
 
 public:
   explicit source() {}
