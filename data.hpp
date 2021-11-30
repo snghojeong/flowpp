0ef2ced9f840bec6d19956f810d1f3258f6570c1
Seungho Jeong <snghojeong@gmail.com>
2021-11-30 16:45:48 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -3,13 +3,14 @@
 
 using data_uptr = std::unique_ptr<data>;
 using data_sptr = std::shared_ptr<data>;
+using format_uptr = std::unique_ptr<format>;
 
 class data {
 public:
   explicit data() { }
   virtual ~data() { }
 
-  virtual const unique_ptr<format> format() const = 0;
+  virtual const format_uptr format() const = 0;
   virtual void set(data& dat) = 0;
 };
 
