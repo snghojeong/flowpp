1013c42732bfc1dcc6ae33dd3a152dd021ea7bcd
Seungho Jeong <snghojeong@gmail.com>
2021-12-10 23:54:57 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -7,8 +7,8 @@ class observer {
   using data_sptr = std::shared_ptr<data>;
 
 public:
-  virtual void notify(data_uptr& dat) = 0;
-  virtual void notify(data_sptr& dat) = 0;
+  virtual void notify(data_uptr& data_ptr) = 0;
+  virtual void notify(data_sptr& data_ptr) = 0;
 };
 
 #endif
