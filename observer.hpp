721e75d821059dbe99ae270cbb0d6e9c3d2ecdf4
Seungho Jeong <snghojeong@gmail.com>
2022-04-27 22:17:12 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -7,7 +7,7 @@ class observer {
   using data_uptr = std::unique_ptr<data>;
 
 public:
-  virtual void notify(data_uptr& data_ptr) = 0;
+  virtual void notify(const data_uptr& data_ptr) = 0;
 };
 
 #endif
