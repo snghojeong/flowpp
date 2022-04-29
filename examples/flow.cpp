d7b50739cc04fa378c325f284a65c64c266ff49f
Seungho Jeong <snghojeong@gmail.com>
2022-04-29 17:54:56 +0900

    Update flow.cpp

--- examples/flow.cpp
+++ examples/flow.cpp
@@ -1,6 +1,7 @@
 using content_type = flowpp::network::http::content_type;
 using port_uptr = std::unique_ptr<flowpp::network::port>;
 using graph_uptr = std::unique_ptr<graph>;
+using tcp_receiver = flowpp::network::tcp_receiver;
 
 void main()
 {
