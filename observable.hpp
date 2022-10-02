cac0ed3c536299e799c5f54ef36fcd99e030dba5
Seungho Jeong <snghojeong@gmail.com>
2022-10-02 22:57:42 +0900

    Update observable.hpp

--- observable.hpp
+++ observable.hpp
@@ -8,7 +8,7 @@ class observable {
   using data_list_uptr = std::unique_ptr<std::list<data_uptr>>;
 
 public:
-  explicit observable() { _obs_map = make_unique<std::map<string, observer_uptr>>() }
+  explicit observable() { _obs_map = make_unique<std::map<string, observer_uptr>>(); _data_list = make_unique<std::list<data_uptr>>(); }
   virtual ~observable() {}
 
   virtual void process() {
