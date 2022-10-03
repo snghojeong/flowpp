4146f6b74d0a54b0d19c5399ab79e391ff1d3fcf
Seungho Jeong <snghojeong@gmail.com>
2022-10-03 21:45:04 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -5,11 +5,11 @@ class data;
 class format;
 
 using data_uptr = std::unique_ptr<data>;
-using format_uptr = std::unique_ptr<format>;
+using fmt_uptr = std::unique_ptr<format>;
 
 class data {
 public:
-  virtual const format_uptr format() const = 0;
+  virtual const fmt_uptr format() const = 0;
   virtual void set(data_uptr dat) = 0;
   virtual std::string name() const = 0;
   virtual data_uptr clone() const = 0;
