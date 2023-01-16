6017c154502ac2aba5591c7707faba5df1d519c2
Seungho Jeong <snghojeong@gmail.com>
2023-01-16 22:23:09 +0900

    Update flow.cpp

--- examples/flow.cpp
+++ examples/flow.cpp
@@ -13,7 +13,7 @@ void main()
   // HTTP
   src["JSON"] | json_builder | http_builder | tcp_tx; 
   tcp_rx[port(80)] |    http_parser[content_type("application/json")] | file_writer("recv.json");
-                        http_parser[content_type("plain/text")] | [] (const std::string& txt) { cout << txt; };
+  tcp_rx[port(8080)] |  http_parser[content_type("plain/text")] | [] (const std::string& txt) { cout << txt; };
 
   // FTP
   tcp_rx[port(22)] | ftp_parser | file_writer("filename");
