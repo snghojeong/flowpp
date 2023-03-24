4bcdeecc77941d6eae5ae3a94e82e4d56beaca73
Seungho Jeong <snghojeong@gmail.com>
2023-03-24 21:52:26 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -6,7 +6,7 @@ using data_uptr = std::unique_ptr<data>;
 class observer {
   
 public:
-  virtual void notify(const data_uptr d) = 0;
+  virtual void notify(const data_uptr dt) = 0;
   virtual void subscribe(observable *obsvl) = 0;
 };
 
