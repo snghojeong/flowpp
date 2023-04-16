82919bed1c1abeb97ce974bdff870c51dd4c390e
Seungho Jeong <snghojeong@gmail.com>
2023-04-16 22:11:29 +0900

    Update graph.hpp

--- graph.hpp
+++ graph.hpp
@@ -11,7 +11,6 @@ public:
   void run(time_t timeout, unsigned long loop) {
     auto start_time = 0;
     auto curr_time = 0;
-    auto cnt = 0;
     while ((loop == 0) || ((cnt++ < loop) && (start_time > curr_time))) {
       std::for_each(_obsvl_list.begin(), _obsvl_list.end(), 
           [] (obsvl_uptr obsbl) { obsbl->emit(); });
