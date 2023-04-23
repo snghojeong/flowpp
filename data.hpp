72b62464efb80716474dba3a5fa05e317250f67b
Seungho Jeong <snghojeong@gmail.com>
2023-04-23 21:24:51 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -1,12 +1,10 @@
 #ifndef _DATA_HPP_
 #define _DATA_HPP_
 
-using data_uptr = std::unique_ptr<data>;
-
 class data <typename T> {
 public:
   virtual const format *format() const = 0;
-  virtual void set(data_uptr dat) = 0;
+  virtual void set(std::unique_ptr<data> dat) = 0;
   virtual T get() const = 0;
   virtual const std::string str() const = 0;
 };
