b0b51aa18d27b5d93c0a23832ed84c120f6d0ad5
Seungho Jeong <snghojeong@gmail.com>
2023-09-20 17:36:39 +0900

    Update hierarchy.cpp

--- examples/hierarchy.cpp
+++ examples/hierarchy.cpp
@@ -4,7 +4,7 @@ using content_type_uptr = unique_ptr<flowpp::network::http::content_type>;
 using json_enc_uptr = unique_ptr<json_encoder>;
 using file_src_uptr = unique_ptr<file_src>;
 
-class http_flow_container : public emitter, public receiver {
+class http_flow_container : public observer, public observable {
 public:
   explicit flow_container(std::string contents_type) {
     http_builder[make_unique<content_type_uptr>(contents_type)] | this;
