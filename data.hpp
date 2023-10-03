71aef6e83302da52bb51720bd7001d04f7a7dc35
Seungho Jeong <snghojeong@gmail.com>
2023-10-03 20:17:43 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -5,7 +5,7 @@ using namespace flowpp;
 
 class data <typename T> {
 public:
-  virtual void set(std::unique_ptr<T> value) = 0;
+  virtual void set(T value) = 0;
   virtual T get() const = 0;
   virtual const std::string str() const = 0;
 };
