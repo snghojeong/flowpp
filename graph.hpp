509ce8f1ad4c7b3157a348592d11b6d3570ede58
Seungho Jeong <snghojeong@gmail.com>
2023-10-30 21:56:09 +0900

    Update graph.hpp

--- graph.hpp
+++ graph.hpp
@@ -10,7 +10,7 @@ public:
   void run(time_t timeout, unsigned long loop) {
     auto start_time = 0;
     auto curr_time = 0;
-    while ((loop == 0) || ((cnt++ < loop) && (start_time > curr_time))) {
+    while ((loop <= 0) || ((cnt++ < loop) && (start_time > curr_time))) {
       std::for_each(_obsvl_list.begin(), _obsvl_list.end(), 
           [] (obsvl_uptr obsbl) { obsbl->emit(); });
       curr_time = millis();
