e482191ca27f2b5a531361411a29a06a4de88aa3
Seungho Jeong <snghojeong@gmail.com>
2023-11-01 22:04:00 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -7,7 +7,7 @@ class data <typename T> {
 public:
   virtual void set(T value) = 0;
   virtual T get() const = 0;
-  virtual const std::string str() const = 0;
+  virtual const std::string to_str() const = 0;
 };
 
 #endif
