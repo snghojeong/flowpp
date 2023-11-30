32d47ec7747142008a4c7017c1dcdcd6bf60abfc
Seungho Jeong <snghojeong@gmail.com>
2023-11-30 23:30:52 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -9,6 +9,7 @@ public:
   virtual T get() const = 0;
   virtual void copy_to(data_uptr dst) const = 0;
   virtual const std::string to_str() const = 0;
+  virtual const bytes_uptr serialize() const = 0;
 };
 
 #endif
