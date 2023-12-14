10c2f0f66ae37c7c811b9c9e509bd9926dadb4fd
Seungho Jeong <snghojeong@gmail.com>
2023-12-14 20:04:26 +0900

    Update hierarchy.cpp

--- examples/hierarchy.cpp
+++ examples/hierarchy.cpp
@@ -3,6 +3,7 @@ using namespace flowpp;
 using content_type_uptr = unique_ptr<flowpp::network::http::content_type>;
 using json_enc_uptr = unique_ptr<json_encoder>;
 using file_src_uptr = unique_ptr<file_src>;
+using data_uptr = unique_ptr<data>;
 
 class http_flow_container : public observer, public observable {
 public:
