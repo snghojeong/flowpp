219f2bab162ac19674c5a8a0925e89770fa111ac
Seungho Jeong <snghojeong@gmail.com>
2024-02-09 20:09:04 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -11,6 +11,7 @@ public:
   virtual T get() const = 0;
   virtual size_t size() const = 0;
   virtual data_uptr clone() const = 0;
+  virtual void parse() = 0;
   virtual const bytes_uptr serialize() const = 0;
   virtual const void deserialize(bytes_uptr bs) = 0;
 };
