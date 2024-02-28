12f7d574d0faa10d0741532f14dfdd8fe5637d2b
Seungho Jeong <snghojeong@gmail.com>
2024-02-28 22:53:44 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -8,7 +8,7 @@ using data_uptr = std::unique_ptr<data>;
 class observer {
 public:
   virtual void subscribe(obsvl_uptr obsvl) = 0;
-  virtual void listen(std::function func) = 0;
+  virtual void listen(std::function lambda) = 0;
   virtual data_uptr poll(uint64_t timeout = 0) = 0;
 };
 
