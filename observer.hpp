a3a686da266787e69e89ea39734bc4240172ecd5
Seungho Jeong <snghojeong@gmail.com>
2024-03-25 20:20:01 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -9,7 +9,7 @@ class observer {
 public:
   virtual void subscribe(obsvl_uptr obsvl) = 0;
   virtual void listen(std::function lambda) = 0;
-  virtual data_uptr poll(uint64_t timeout = 0) = 0;
+  virtual data_uptr<typename T> poll(uint64_t timeout = 0) = 0;
 };
 
 #endif
