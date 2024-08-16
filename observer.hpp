7e7531a6d4ea143f61d0f183f0a429e5c106ca30
Seungho Jeong <snghojeong@gmail.com>
2024-08-16 11:23:43 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -3,13 +3,13 @@ using namespace flowpp;
 #ifndef _OBSERVER_HPP_
 #define _OBSERVER_HPP_
 
-using data_uptr<typename T> = std::unique_ptr<data<T>>;
-
+template <typename T>
 class observer {
+using data_uptr = std::unique_ptr<data<T>>;
 public:
   virtual void subscribe(obsvl_uptr obsvl) = 0;
   virtual void listen(std::function lambda) = 0;
-  virtual data_uptr<typename T> poll(uint64_t timeout = 0) = 0;
+  virtual data_uptr poll(uint64_t timeout = 0) = 0;
 };
 
 #endif
