5db998895034e22cc5568ab86632187f121c89e8
Seungho Jeong <snghojeong@gmail.com>
2024-08-20 16:30:46 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -9,14 +9,14 @@ using std::unique_ptr<std::string> str_uptr;
 using std::unique_ptr<std::bitstream> bs_uptr;
 public:
   data() { }
-  virtual void set(T value) = 0;
+  virtual void set(const T value) = 0;
   virtual T get() const = 0;
   virtual size_t size() const = 0;
   virtual data_uptr clone() const = 0;
-  virtual void parse(str_uptr str) = 0;
+  virtual void parse(const str_uptr str) = 0;
   virtual str_uptr to_str() = 0;
   virtual const bs_uptr serialize() const = 0;
-  virtual const void deserialize(bs_uptr bs) = 0;
+  virtual const void deserialize(const bs_uptr bs) = 0;
 };
 
 #endif
