9ee61ddad342b0ff2405cc5ecc6b5d8687485e39
Seungho Jeong <snghojeong@gmail.com>
2024-08-28 22:09:17 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -5,12 +5,10 @@ using namespace flowpp;
 
 template <typename T>
 class observer {
-using data_uptr = std::unique_ptr<data<T>>;
 using obsvl_uptr = std::unique_ptr<observable<T>>;
 public:
   virtual void subscribe(obsvl_uptr obsvl) = 0;
   virtual void listen(std::function callback) = 0;
-  virtual data_uptr poll(uint64_t timeout = 0) = 0;
 };
 
 #endif
