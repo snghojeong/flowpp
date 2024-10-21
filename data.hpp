b84625b1db7d3bab9fad2a375bdd9f57bcbe27e5
Seungho Jeong <snghojeong@gmail.com>
2024-10-21 20:23:49 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -1,22 +1,30 @@
-using namespace flowpp;
-
 #ifndef _DATA_HPP_
 #define _DATA_HPP_
 
+#include <memory>
+#include <string>
+#include <bitset>
+
 template <typename T>
 class data {
-using std::unique_ptr<std::string> str_uptr;
-using std::unique_ptr<std::bitstream> bs_uptr;
+    using StrPtr = std::unique_ptr<std::string>;
+    using BsPtr = std::unique_ptr<std::bitset<8>>; // Assuming 8-bit streams for the example
+
 public:
-  data() { }
-  virtual void set(const T value) = 0;
-  virtual T get() const = 0;
-  virtual size_t size() const = 0;
-  virtual data_uptr clone() const = 0;
-  virtual void parse(const str_uptr str) = 0;
-  virtual str_uptr to_str() = 0;
-  virtual const bs_uptr serialize() const = 0;
-  virtual const void deserialize(const bs_uptr bs) = 0;
+    data() = default;
+    virtual ~data() = default;
+
+    // Pure virtual methods for derived classes to implement
+    virtual void set(const T& value) = 0;
+    virtual T get() const = 0;
+    virtual size_t size() const = 0;
+    virtual std::unique_ptr<data<T>> clone() const = 0;
+    
+    // Methods for parsing and serialization
+    virtual void parse(const StrPtr& str) = 0;
+    virtual StrPtr to_str() const = 0;
+    virtual BsPtr serialize() const = 0;
+    virtual void deserialize(const BsPtr& bs) = 0;
 };
 
-#endif
+#endif // _DATA_HPP_
