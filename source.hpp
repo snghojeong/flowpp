1177d6b25195b215376d275631204b57774222ae
Seungho Jeong <snghojeong@gmail.com>
2024-10-24 19:42:56 +0900

    Update source.hpp

--- source.hpp
+++ source.hpp
@@ -1,19 +1,24 @@
-using namespace flowpp;
-
 #ifndef _SOURCE_HPP_
 #define _SOURCE_HPP_
 
-class data;
+#include <memory>
+#include <functional>
+#include "observable.hpp"  // Assuming this is the observable base class
 
 template <typename T>
-class source : public observable {
-using data_uptr = std::unique_ptr<data<T>>;
+class source : public observable<T> {
+    using DataPtr = std::unique_ptr<data<T>>;
+
 public:
-  explicit source() {}
-  virtual ~source() {}
+    // Constructor and destructor
+    explicit source() = default;
+    virtual ~source() = default;
 
-  data_uptr poll(uint64_t timeout) = 0;
-  void listen(std::function callback) = 0;
-}
-#endif
+    // Pure virtual function for polling data
+    virtual DataPtr poll(uint64_t timeout) override = 0;
 
+    // Pure virtual function for setting up a listener
+    virtual void listen(const std::function<void(const DataPtr&)>& callback) override = 0;
+};
+
+#endif // _SOURCE_HPP_
