c1e4f2af4472d63d12299c5defc485ac5dde51fb
Seungho Jeong <snghojeong@gmail.com>
2024-10-25 17:02:37 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -1,14 +1,22 @@
-using namespace flowpp;
-
 #ifndef _OBSERVER_HPP_
 #define _OBSERVER_HPP_
 
+#include <memory>
+#include <functional>
+#include "observable.hpp"  // Ensure this header is included for observable<T>
+
 template <typename T>
 class observer {
-using obsvl_uptr = std::unique_ptr<observable<T>>;
+    using ObservablePtr = std::unique_ptr<observable<T>>;
+
 public:
-  virtual void subscribe(obsvl_uptr obsvl) = 0;
-  virtual void listen(std::function callback) = 0;
+    virtual ~observer() = default;
+
+    // Pure virtual method to subscribe to an observable
+    virtual void subscribe(ObservablePtr observable) = 0;
+
+    // Pure virtual method to listen with a callback function
+    virtual void listen(const std::function<void(const std::unique_ptr<data<T>>& data)>& callback) = 0;
 };
 
-#endif
+#endif // _OBSERVER_HPP_
