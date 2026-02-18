142c0a05f3cff47df7beed3c26f5e5af4a06ace8
Seungho Jeong <snghojeong@gmail.com>
2026-02-18 11:45:27 +0900

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -1,25 +1,26 @@
 #pragma once
 
 #include <memory>
-#include <string>
 #include <bitset>
 
 template <typename T>
-class data {
-    using StrPtr = std::unique_ptr<std::string>;
-    using BsPtr = std::unique_ptr<std::bitset<8>>;
-
+class IData {
 public:
-    data() = default;
-    data(const T& value);
-    data(BsPtr bs);
-    virtual ~data() = default;
+    // Using a standard alias for the serialization format
+    using BitBlock = std::bitset<8>;
 
+    virtual ~IData() = default;
+
+    // Core Data Access
     virtual void set(const T& value) = 0;
     virtual T get() const = 0;
     virtual size_t size() const = 0;
-    virtual std::unique_ptr<data<T>> clone() const = 0;
-    virtual std::unique_ptr<data<T>> move() = 0;
-    virtual BsPtr serialize() const = 0;
-    virtual void deserialize(const BsPtr& bs) = 0;
+
+    // Serialization: Use values or references instead of unique_ptr 
+    // for small objects like bitset<8> to avoid heap overhead.
+    virtual BitBlock serialize() const = 0;
+    virtual void deserialize(const BitBlock& bs) = 0;
+
+    // Polymorphic Cloning
+    virtual std::unique_ptr<IData<T>> clone() const = 0;
 };
