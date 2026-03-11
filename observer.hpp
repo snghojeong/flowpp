7558267acd27d3cc1b99e2cc46384f61ddba5fe7
Seungho Jeong <snghojeong@gmail.com>
2026-03-11 17:30:30 +0900

    Update observer.hpp

--- observer.hpp
+++ observer.hpp
@@ -7,13 +7,11 @@
 template <typename T>
 class observer {
 public:
-    // Simplifies long function signatures
     using ObservablePtr = std::shared_ptr<observable<T>>;
     using DataCallback = std::function<void(const data<T>&)>;
 
     virtual ~observer() = default;
 
-    // Pure virtual interface
     virtual void subscribe(ObservablePtr subject) = 0;
     virtual void listen(DataCallback callback) = 0;
 };
